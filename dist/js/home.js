fe13428263340d43bc9bf4867c02e63a8742d919
Derek Hon <derek.hon@outlook.com>
2021-06-04 15:21:05 -0400

    Refactoring entire site to a different format of UI

--- dist/js/home.js
+++ dist/js/home.js
@@ -1,27 +1,36 @@
-$(function () {
-    jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000  // 2 seconds
+// $(function () {
+//     jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000  // 2 seconds
 
-    $("#aPhoto").click(function () {
-        $('html, body').animate({
-            scrollTop: $("#photo").offset().top
-        }, 1000);
-    });
+//     $("#aPhoto").click(function () {
+//         $('html, body').animate({
+//             scrollTop: $("#photo").offset().top
+//         }, 1000);
+//     });
 
-    $("#aArt").click(function () {
-        $('html, body').animate({
-            scrollTop: $("#art").offset().top
-        }, 1000);
-    });
+//     $("#aArt").click(function () {
+//         $('html, body').animate({
+//             scrollTop: $("#art").offset().top
+//         }, 1000);
+//     });
 
-    $("#aCS").click(function () {
-        $('html, body').animate({
-            scrollTop: $("#cs").offset().top
-        }, 1000);
-    });
-    $("#aAbout").click(function () {
-        console.log("here");
-        $('html, body').animate({
-            scrollTop: $("#about").offset().top
-        }, 1000);
-    });
+//     $("#aCS").click(function () {
+//         $('html, body').animate({
+//             scrollTop: $("#cs").offset().top
+//         }, 1000);
+//     });
+//     $("#aAbout").click(function () {
+//         console.log("here");
+//         $('html, body').animate({
+//             scrollTop: $("#about").offset().top
+//         }, 1000);
+//     });
+// });
+
+$(document).ready(function() {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+   });
+
+   
+
 });
