99b8a68cff81aa9db661250efd60e3c2a001853f
Derek Hon <derek.hon@outlook.com>
2021-08-23 18:35:54 -0400

    Updates

--- js/home.js
+++ js/home.js
@@ -32,31 +32,47 @@ $(document).ready(function() {
 //    });
 
    $("#menuAbout").click(function() {
-       $("#content").find(".live").fadeOut("fast");
-       $("#content").find(".live").removeClass("live");
-       $("#about").fadeIn("fast");
-       $("#about").addClass("live");
+       if ($(this) != $(this).parent().find(".active")) {
+          $("#content").find(".live").fadeOut("fast");
+          $("#content").find(".live").removeClass("live");
+          $("#about").fadeIn("fast");
+          $("#about").addClass("live");
+          $(this).parent().find(".active").removeClass("active");
+          $(this).addClass("active");
+       }
    })
 
    $("#menuCode").click(function() {
-      $("#content").find(".live").fadeOut("fast");
-      $("#content").find(".live").removeClass("live");
-      $("#cs").fadeIn("fast");
-      $("#cs").addClass("live");
+      if ($(this) != $(this).parent().find(".active")) {
+        $("#content").find(".live").fadeOut("fast");
+        $("#content").find(".live").removeClass("live");
+        $("#cs").fadeIn("fast");
+        $("#cs").addClass("live");
+        $(this).parent().find(".active").removeClass("active");
+        $(this).addClass("active");
+      }
   })
 
   $("#menuPhoto").click(function() {
+    if ($(this) != $(this).parent().find(".active")) {
       $("#content").find(".live").fadeOut("fast");
       $("#content").find(".live").removeClass("live");
       $("#photo").fadeIn("fast");
       $("#photo").addClass("live");
+      $(this).parent().find(".active").removeClass("active");
+      $(this).addClass("active");
+    }
   })
 
   $("#menuArt").click(function() {
+    if ($(this) != $(this).parent().find(".active")) {
       $("#content").find(".live").fadeOut("fast");
       $("#content").find(".live").removeClass("live");
       $("#art").fadeIn("fast");
       $("#art").addClass("live");
+      $(this).parent().find(".active").removeClass("active");
+      $(this).addClass("active");
+    }
   })
 
    var sticky = $("#sidebar").offset.top;
